0f38fc3bf6f174bdcbb9e56ce79f0d829bed1868
DevTest <115026138+adrien-pago@users.noreply.github.com>
2024-04-15 14:00:45 +0200

    SQL Projet de base

---
+++ Quizz/Joueur.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz
+{
+    public class Joueur
+    {
+        public Joueur()
+        {
+
+        }
+
+        public string Pseudo { get; set; }
+        public int Score { get; set; }
+
+        public override string ToString()
+        {
+            string chaine = Pseudo + "- Score: " + Score;
+            return chaine;
+        }
+
+    }
+
+    
+}
---
+++ Quizz/Quizz/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz
+{
+    public class Question
+    {
+        public Question()
+        {
+
+        }
+        public string Categories { set; get; }
+        public string NomQuestion { set; get; }
+        public string ReponseA { set; get; }
+        public string ReponseB { set; get; }
+        public string ReponseC { set; get; }
+        public int BonneReponse { set; get; }
+    }
+}
